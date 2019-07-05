6d675492b46ae42c7c4c0c1f4a10578d785ed132
Cristhian Camilo Gomez Neira <ccgomezn@unal.edu.co>
2019-07-04 23:48:42 -0500

    confirm user activation

--- app/controllers/user_token_controller.rb
+++ app/controllers/user_token_controller.rb
@@ -1,2 +1,13 @@
 class UserTokenController < Knock::AuthTokenController
+ skip_before_action :verify_authenticity_token
+
+  def create
+    user = User.find_by email: auth_params[:email]
+    p user.user_type
+    if user.user_type == 0 and user.state == false
+      return render json: {message: 'User not activated'}, status: 403
+    end
+
+    super
+  end
 end
