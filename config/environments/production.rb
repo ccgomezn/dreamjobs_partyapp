8e082c2973be2989ad46a9f0d48d413506592d33
Cristhian Camilo Gomez Neira <ccgomezn@unal.edu.co>
2019-07-05 14:52:11 -0500

    changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,26 @@ Rails.application.configure do
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
+  config.action_mailer.raise_delivery_errors = false
 
+  config.action_mailer.perform_caching = false
+  
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            Rails.application.credentials.gmail_user,
+    password:             Rails.application.credentials.gmail_password,
+    authentication:       'plain',
+    enable_starttls_auto: true }
+
+  # Raise an error on page load if there are pending migrations.
+  config.active_record.migration_error = :page_load
+
+  # Highlight code that triggered database queries in logs.
+  config.active_record.verbose_query_logs = true
+  config.action_mailer.default_url_options = { :host => "partyappdreamjobs.com" }
   # Send deprecation notices to registered listeners.
   config.active_support.deprecation = :notify
 
